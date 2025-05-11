8913e14fc0cdd7215307e5f46fc0f393c68f53ea
Gustavo Hennings <gustavo.hennings@hotmail.com>
2025-05-10 23:06:13 -0300

    fix(react): add missing key prop in list rendering

--- components/techs-section/TechsSection.tsx
+++ components/techs-section/TechsSection.tsx
@@ -16,8 +16,8 @@ export const TechSection = ({techs}: KnownTechsProps) => {
             <SectionTitle subtitle="competências" title="Conhecimentos"></SectionTitle>
             <div className="grid grid-cols-[repeat(auto-fit,minmax(264px,1fr))] gap-3 mt-[60px]">
                 {techs?.map((tech, i) => (
-                    <motion.div initial={{opacity: 0, x: -100}} whileInView={{opacity: 1, x: 0}} exit={{opacity: 0, x: -100}} transition={{duration: 0.15, delay: i * 0.1}}>
-                        <KnownTech key={tech.name} tech={tech} />
+                    <motion.div  key={tech.name} initial={{opacity: 0, x: -100}} whileInView={{opacity: 1, x: 0}} exit={{opacity: 0, x: -100}} transition={{duration: 0.15, delay: i * 0.1}}>
+                        <KnownTech tech={tech} />
                     </motion.div>
                 ))}
             </div>
