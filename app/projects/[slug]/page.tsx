6c1c4de06172f468a214d4bd9676e1d5a8781f6d
Gustavo Hennings <gustavo.hennings@hotmail.com>
2025-05-10 22:53:28 -0300

    Fix: Corrected params type mismatch in ProjectProps

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -3,11 +3,10 @@ import { ProjectPageData, ProjectsPageStaticData } from "@/app/types/pageInfo";
 import { ProjectDetails } from "@/components/projects/ProjectDetails";
 import { ProjectSections } from "@/components/projects/ProjectSections";
 import { Metadata } from "next";
+import { notFound } from "next/navigation";
 
 type ProjectProps = {
-  params: {
-    slug: string;
-  };
+  params: Promise<{ slug: string }>;
 };
 
 
@@ -29,7 +28,7 @@ export async function generateStaticParams() {
 }
 
 const getProjectDetails = async (slug: string): Promise<ProjectPageData> => {
-    const query = `
+  const query = `
   query ProjectQuery {
     project(where: { slug: "${slug}" }) {
       pageThumbnail {
@@ -58,37 +57,46 @@ const getProjectDetails = async (slug: string): Promise<ProjectPageData> => {
     }
   }
 `
-  
-    return fetchHygraphQuery(query, 0)
-  }
-  export default async function Project({ params: { slug } }: ProjectProps) {
-    const { project } = await getProjectDetails(slug)
-  
-    return (
-      <>
-        <ProjectDetails project={project} />
-        <ProjectSections sections={project.sections} />
-      </>
-    )
-  }
 
-  export async function generateMetadata({
-    params: {slug}
-  }: ProjectProps): Promise<Metadata>{
-    const data = await getProjectDetails(slug)
-    const project = data.project
-    return{
-      title: project.title,
-      description: project.description.text,
-      openGraph: {
-        images: [
-          {
-            url: project.thumbnail.url,
-            width: 1200,
-            height: 630,
-          }
-        ]
-      }
+  return fetchHygraphQuery(query, 0)
+}
+export default async function Project(props: ProjectProps) {
+  const params = await props.params; // Aguarde a resolução do Promise
+  const slug = params.slug;
+
+  const { project } = await getProjectDetails(slug);
+
+  if (!project?.title) return notFound();
+
+  return (
+    <>
+      <ProjectDetails project={project} />
+      <ProjectSections sections={project.sections} />
+    </>
+  );
+}
+
+
+export async function generateMetadata({
+  params
+}: ProjectProps): Promise<Metadata> {
+  const resolvedParams = await params; // Resolve o Promise
+  const slug = resolvedParams.slug;
+
+  const data = await getProjectDetails(slug);
+  const project = data.project;
+
+  return {
+    title: project.title,
+    description: project.description.text,
+    openGraph: {
+      images: [
+        {
+          url: project.thumbnail.url,
+          width: 1200,
+          height: 630,
+        }
+      ]
     }
-  }
-  
+  };
+}
